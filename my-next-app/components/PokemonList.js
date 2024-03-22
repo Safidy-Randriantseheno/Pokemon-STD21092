a649e79461eff7423b938f7cf508b3fa69c1e76f
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-03-22 11:04:06 +0300

    feat: implement pokemon list .

---
+++ my-next-app/components/PokemonList.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+const PokemonList = () => {
+ const router = useRouter();
+ const [pokemons, setPokemons] = useState([]);
+ const [currentPage, setCurrentPage] = useState(1);
+ const [totalPages, setTotalPages] = useState(0);
+
+ useEffect(() => {
+    fetchPokemons(currentPage);
+ }, [currentPage]);
+
+ const fetchPokemons = async (page) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${(page - 1) * 50}`);
+    const data = await response.json();
+    setPokemons(data.results);
+    setTotalPages(Math.ceil(data.count / 50));
+ };
+
+ return (
+    <div>
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <li key={index}>
+            {pokemon.name}
+            <button onClick={() => router.push(`/${router.pathname.split('/')[1]}/${pokemon.name}`)}>Détails</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        {currentPage > 1 && <button onClick={() => setCurrentPage(currentPage - 1)}>Précédent</button>}
+        {currentPage < totalPages && <button onClick={() => setCurrentPage(currentPage + 1)}>Suivant</button>}
+      </div>
+    </div>
+ );
+};
+
+export default PokemonList;
