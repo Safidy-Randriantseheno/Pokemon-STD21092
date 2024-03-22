6e6f78d8ca619e21b8848fd206598aa5069e8bf4
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-03-22 11:02:16 +0300

    feat: initiating project.

---
+++ my-next-app/pages/client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const ClientPage = () => {
+ const router = useRouter();
+
+ return (
+    <div>
+      <h1>Page client</h1>
+
+    </div>
+ );
+};
+
+export default ClientPage;
---
+++ my-next-app/pages/server/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const ServerPage = () => {
+ const router = useRouter();
+
+ return (
+    <div>
+      <h1>Page Serveur</h1>
+
+    </div>
+ );
+};
+
+export default ServerPage;
