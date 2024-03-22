960fc7a1da9879bcc93ef67210611ccdbef73873
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-03-22 11:22:55 +0300

    feat: Use the reusable component.

---
+++ my-next-app/pages/client/[id].js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokemonDetails from '../../components/PokemonDetails';
+
+const ClientPokemonDetails = () => {
+ return (
+    <div>
+      <h1>Détails du Pokémon - Client</h1>
+      <PokemonDetails />
+    </div>
+ );
+};
+
+export default ClientPokemonDetails;
---
+++ my-next-app/pages/server/[id].js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokemonDetails from '../../components/PokemonDetails';
+
+const ServerPokemonDetails = () => {
+ return (
+    <div>
+      <h1>Détails du Pokémon - Serveur</h1>
+      <PokemonDetails />
+    </div>
+ );
+};
+
+export default ServerPokemonDetails;
